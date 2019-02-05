9862b35e20d1542e766951b698995022cc1b38b4
Matt Breckon <mattbreckon@gmail.com>
2019-02-05 01:16:40 -0500

    Windows close button styling

--- src/components/Window/WindowButton/WindowButton.js
+++ src/components/Window/WindowButton/WindowButton.js
@@ -5,8 +5,26 @@ import '../Window.scss';
 export default class WindowButton extends React.Component {
   static displayName = 'WindowButton';
 
+  constructor() {
+    super();
+
+    this.state = {
+      hover: false
+    };
+  }
+
+  onMouseEnter = () => {
+    this.setState({ hover: true });
+  }
+
+  onMouseLeave = () => {
+    this.setState({ hover: false });
+  }
+
   render() {
-    const className = 'button' + (this.props.color ? ` button--${this.props.color}` : '');
+    const className = 'button' +
+      (this.props.color ? ` button--${this.props.color}` : '') +
+      (this.state.hover ? ' hover' : '');
 
     const onHandlers = {
       onMouseEnter: this.onMouseEnter,
