fe374590298ff7d6d428472da574ef5b8fb30a6f
Matt Breckon <mattbreckon@gmail.com>
2019-02-05 00:47:24 -0500

    Mobile and no script message + power on pulse

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import App from './components/App';
 import './scss/index.scss';
 
+
 ReactDOM.render(
-  <App />,
+  // naive User Agent detection
+  (/iPhone|iPad|iPod|Android/i.test(navigator.userAgent) ?
+    <div className='mobile'>This website is not optimized for mobile.</div> :
+    <App />
+  ),
   document.getElementById('root')
 );
